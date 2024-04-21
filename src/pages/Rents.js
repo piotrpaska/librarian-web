851f96f095669b19290f914af46e63207c3f99d3
Piotr Paśka <paska.piotrek@gmail.com>
2024-04-21 21:25:49 +0200

    Edit rent modal and set default modal form values

--- src/pages/Rents.js
+++ src/pages/Rents.js
@@ -91,6 +91,37 @@ function Rents() {
     });
   }
 
+  const handleEditRent = async (selectedId) => {
+    var rentData;
+
+    try {
+      const response = await api.get(`/one-rent/${selectedId}`);
+      rentData = response.data;
+      console.log(rentData);
+    } catch (error) {
+      console.log(error);
+    }
+
+    const name = $('#name-edit');
+    const lastName = $('#lastName-edit');
+    const schoolClass = $('#schoolClass-edit');
+    const bookTitle = $('#bookTitle-edit');
+    const isDeposit = $('#isDeposit-edit');
+    const deposit = $('#deposit-edit');
+    const rentalDate = $('#rentalDate-edit');
+    const maxDate = $('#maxDate-edit');
+
+    name.val(rentData.name);
+    lastName.val(rentData.lastName);
+    schoolClass.val(rentData.schoolClass);
+    bookTitle.val(rentData.bookTitle);
+    isDeposit.prop('checked', rentData.isLongRent);
+    deposit.val(rentData.deposit);
+    rentalDate.val(rentData.rentDate);
+    maxDate.val(rentData.dueDate);
+
+  }
+
   function search() {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue, filterBy;
@@ -194,10 +225,19 @@ function Rents() {
                   <td>{rent.rentDate}</td>
                   <td>{rent.dueDate}</td>
                   <td>{calculateRentStatus(rent.rentDate, rent.dueDate, rent.isLongRent)}</td>
-                  <td><button className='btn btn-primary btn-sm' data-bs-toggle="modal" data-bs-target="#confirmDeleteMod" id={rent.id} onClick={() => handleEndRent(rent.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16</svg>">
-                      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                    </svg></button></td>
+                  <td>
+                    <button className='btn btn-primary btn-sm me-1' data-bs-toggle="modal" data-bs-target="#confirmDeleteMod" id={rent.id} onClick={() => handleEndRent(rent.id)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16</svg>">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                      </svg>
+                    </button>
+                    <button className='btn btn-primary btn-sm ms-1' data-bs-toggle="modal" data-bs-target="#editRentModal" onClick={() => handleEditRent(rent.id)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                      </svg>
+                    </button>
+                  </td>
                 </tr>
               )
 
@@ -205,7 +245,7 @@ function Rents() {
           </tbody>
         </table>
       </div>
-
+      
       <div class="modal fade" tabindex="-1" id='confirmDeleteMod'>
         <div class="modal-dialog">
           <div class="modal-content">
@@ -223,12 +263,12 @@ function Rents() {
           </div>
         </div>
       </div>
-
+      
       <div class="modal fade" id="addRentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Nowe wypożyczenie</h1>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id='addRentForm' onSubmit={onSubmitForm}>
@@ -271,6 +311,65 @@ function Rents() {
                 <div class="mb-3">
                   <label for="maxDate" class="form-label">Termin</label>
                   <input type="date" class="form-control" id="maxDate" name='dueDate' value={dueDate} onChange={handelIsDepositChange} />
+                </div>
+
+              </div>
+              <div class="modal-footer">
+                <button type="button" id='modalCancel' class="btn btn-secondary" data-bs-dismiss="modal">Anuluj</button>
+                <button type="submit" id='modalSubmit' class="btn btn-primary">Zatwierdź</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      
+      <div class="modal fade" id="editRentModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Edycja wypożyczenia</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id='editRentForm' onSubmit={onSubmitForm}>
+              <div class="modal-body text-start">
+
+                <div class="mb-3">
+                  <label for="name" class="form-label">Imię</label>
+                  <input type="text" class="form-control" id="name-edit" required />
+                </div>
+                <div class="mb-3">
+                  <label for="lastName" class="form-label">Nazwisko</label>
+                  <input type="text" class="form-control" id="lastName-edit" required />
+                </div>
+                <div class="mb-3">
+                  <label for="schoolClass" class="form-label">Klasa</label>
+                  <input type="text" class="form-control" id="schoolClass-edit" required />
+                </div>
+                <div class="mb-3">
+                  <label for="bookTitle" class="form-label">Tytuł książki</label>
+                  <input type="text" class="form-control" id="bookTitle-edit" required />
+                </div>
+                <div class="row mb-3 align-items-center">
+                  <label for="deposit" class="form-label">Kaucja</label>
+                  <div className='col pe-0'>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="isDeposit-edit" name='isDeposit' onChange={handelIsDepositChange} />
+                      <label class="form-check-label" for="flexCheckDefault">
+                        Wypożyczenie z kaucją?
+                      </label>
+                    </div>
+                  </div>
+                  <div className='col ps-0'>
+                    <input type="number" class="form-control" id="deposit-edit" placeholder='Tylko cyfra' disabled required />
+                  </div>
+                </div>
+                <div class="mb-3">
+                  <label for="rentalDate" class="form-label">Data wypożyczenia</label>
+                  <input type="date" class="form-control" id="rentalDate-edit" value={new Date().toISOString().split('T')[0]} />
+                </div>
+                <div class="mb-3">
+                  <label for="maxDate" class="form-label">Termin</label>
+                  <input type="date" class="form-control" id="maxDate-edit" name='dueDate' value={dueDate} onChange={handelIsDepositChange} />
                 </div>
 
               </div>
